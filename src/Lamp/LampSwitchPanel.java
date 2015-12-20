6c1bebccfc9f3f9f4cf4a45645ebe18bffe71474
2025989 <anon@git.hub>
2015-12-20 17:26:30 +0100

    efficiency update

--- src/Lamp/LampSwitchPanel.java
+++ src/Lamp/LampSwitchPanel.java
@@ -9,58 +9,30 @@ import java.awt.event.*;
  */
 public class LampSwitchPanel extends JPanel implements ActionListener {
     
-    private JButton lampButton1, lampButton2, lampButton3;
-    private Lamp lamp1, lamp2, lamp3;
+    private JButton[] lampButton = new JButton[3];
+    private Lamp[] lamp = new Lamp[lampButton.length];
     
     public LampSwitchPanel() {
-        lampButton1 = new JButton("aan");
-        add(lampButton1);
-        lampButton1.addActionListener(this);
-        lamp1 = new Lamp(100);
-        lamp1.maakZichtbaar();
-        
-        lampButton2 = new JButton("aan");
-        add(lampButton2);
-        lampButton2.addActionListener(this);
-        lamp2 = new Lamp(200);
-        lamp2.maakZichtbaar();
-        
-        lampButton3 = new JButton("aan");
-        add(lampButton3);
-        lampButton3.addActionListener(this);
-        lamp3 = new Lamp(300);
-        lamp3.maakZichtbaar();
+        for (int i = 0; i < lampButton.length; i++) {
+            lampButton[i] = new JButton("aan");
+            add(lampButton[i]);
+            lampButton[i].addActionListener(this);
+            lamp[i] = new Lamp((i+1)*100);
+            lamp[i].maakZichtbaar();
+        }
     }
     
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == lampButton1) {
-            if (lampButton1.getText().equals("aan")) {
-                lamp1.aan();
-                lampButton1.setText("uit");
-            }
-            else {
-                lamp1.uit();
-                lampButton1.setText("aan");
-            }
-        }
-        if (e.getSource() == lampButton2) {
-            if (lampButton2.getText().equals("aan")) {
-                lamp2.aan();
-                lampButton2.setText("uit");
-            }
-            else {
-                lamp2.uit();
-                lampButton2.setText("aan");
-            }
-        }
-        if (e.getSource() == lampButton3) {
-            if (lampButton3.getText().equals("aan")) {
-                lamp3.aan();
-                lampButton3.setText("uit");
-            }
-            else {
-                lamp3.uit();
-                lampButton3.setText("aan");
+        for (int i = 0; i < lampButton.length; i++) {
+            if (e.getSource() == lampButton[i]) {
+                if (lampButton[i].getText().equals("aan")) {
+                    lamp[i].aan();
+                    lampButton[i].setText("uit");
+                }
+                else {
+                    lamp[i].uit();
+                    lampButton[i].setText("aan");
+                }
             }
         }
     }
